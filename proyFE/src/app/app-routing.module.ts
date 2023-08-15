992e432a2b5818172809d4ac4f954d946a02eec9
juliegalarza <julie.galarza@epn.edu.ec>
2023-08-14 22:27:14 -0500

    inicio

--- proyFE/src/app/app-routing.module.ts
+++ proyFE/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { DetalleVueloComponent } from './detalle-vuelo/detalle-vuelo.component';
 
 const routes: Routes = [
   {path: '', component: PaginaPrincipalComponent},
   {path: 'inicio', component: PaginaPrincipalComponent},
+
+  {path: 'detalle-vuelo',component:DetalleVueloComponent},
+
   {path: '**', component: PageNoFoundComponent}
 ];
 
