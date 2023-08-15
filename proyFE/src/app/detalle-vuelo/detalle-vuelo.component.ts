d812ffd39d7e50212f1423b6b86b5f2ddba48838
juliegalarza <julie.galarza@epn.edu.ec>
2023-08-14 23:56:29 -0500

    detalle-vuelo

--- proyFE/src/app/detalle-vuelo/detalle-vuelo.component.ts
+++ proyFE/src/app/detalle-vuelo/detalle-vuelo.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-detalle-vuelo',
   templateUrl: './detalle-vuelo.component.html',
   styleUrls: ['./detalle-vuelo.component.css']
 })
-export class DetalleVueloComponent {
+export class DetalleVueloComponent  {
 
+  tipoVuelo: string = 'idaVuelta'; // Supongamos que "idaVuelta" es el valor predeterminado
+    
+  // Ejemplo:
+  // Detalles de ida
+  origenIda: string = 'UIO';
+  destinoIda: string = 'GYE';
+  fechaVueloIda: string = '2023-08-20';
+  horaSalidaIda: string = '08:00 AM';
+  horaLlegadaIda: string = '10:00 AM';
+  numeroAvionIda: string = 'ABC123';
+  tipoClaseIda: string = 'Económica';
+  costoBoletoIda: number = 150.00;
+
+  // Detalles de vuelta
+  origenVuelta: string = 'GYE';
+  destinoVuelta: string = 'UIO';
+  fechaVueloVuelta: string = '2023-08-25';
+  horaSalidaVuelta: string = '02:00 PM';
+  horaLlegadaVuelta: string = '04:00 PM';
+  numeroAvionVuelta: string = 'XYZ789';
+  tipoClaseVuelta: string = 'Primera Clase';
+  costoBoletoVuelta: number = 250.00;
+
+
+  totalReserva: number = 400.00; // Ejemplo de total, reemplaza con tu lógica real
+  
 }
